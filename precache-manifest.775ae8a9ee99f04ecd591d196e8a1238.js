c0073bc93a03b00f0f8e59529a7f395d4596aae8
KetanReddy <KetanReddy@users.noreply.github.com>
2021-07-28 01:08:50 +0000

    deploy: 7d8d86f8210f7fa98a9f426e1bc6e27f5e28178c

---
+++ precache-manifest.775ae8a9ee99f04ecd591d196e8a1238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15180942f1f70395f73",
+    "url": "/css/app.2c6e5ac4.css"
+  },
+  {
+    "revision": "f9e9f34479af13b1a1d544e7ff9b0b19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15180942f1f70395f73",
+    "url": "/js/app.1a7c6ed6.js"
+  },
+  {
+    "revision": "bac80f3bd24863912c4c",
+    "url": "/js/chunk-vendors.5c297f35.js"
+  },
+  {
+    "revision": "59710c9e29496b38ed8a9953b0f13841",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
